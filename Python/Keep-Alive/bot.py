53e131e96da676668b025086e71528406f482c28
noSkidlol <119862848+noSkidlol@users.noreply.github.com>
2022-12-19 21:33:26 +0800

    Update and rename index.js to bot.py

---
+++ Python/Keep-Alive/bot.py
@@ -0,0 +1,4 @@
+import keep_alive
+
+keep_alive.keep_alive()
+# after executed keep alive and your code in replit it will create a website copy it and go to uptimerobot.com
--- Python/Keep-Alive/index.js
+++
@@ -1,4 +0,0 @@
-import keep_alive
-
-keep_alive.keep_alive()
-// after executed keep alive and your code in replit it will create a website copy it and go to uptimerobot.com
